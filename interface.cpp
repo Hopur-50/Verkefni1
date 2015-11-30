9b86fc0cdc02918c86fbe6a9149e2f9ab99e8945
fanney83 <fanneyv12@ru.is>
2015-11-30 13:01:48 +0000

    interfaceClass

---
+++ interface.cpp
@@ -0,0 +1,35 @@
+#include "interface.h"
+
+Interface::Interface()
+{
+
+}
+void menu()
+{
+
+}
+void addNew()
+{
+
+}
+{
+void displayList()
+{
+
+}
+void selectOrder()
+{
+
+}
+void search()
+{
+
+}
+void edit()
+{
+
+}
+void delete()
+{
+
+}
---
+++ interface.h
@@ -0,0 +1,21 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
+
+
+class Interface
+{
+public:
+    Interface();
+    void menu();
+    void addNew();
+    void displayList();
+    void selectOrder();
+    void search();
+    void edit();
+    void deleteInfo();
+
+private:
+
+};
+
+#endif // INTERFACE_H
